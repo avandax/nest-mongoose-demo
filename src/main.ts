15aba37ed6b57f906c72f5f6f8675b77895fd34c
avandax <zjjin20@163.com>
2022-05-14 16:39:46 +0800

    feat: 调整host,port 取值方式

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log("Application is running on:", await app.getUrl());
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
+  const configService = app.get<ConfigService>(ConfigService);
+  await app.listen(configService.get<number>("port"), configService.get<string>("hostname"));
+  console.log(`Application is running on: ${await app.getUrl()}, env:${process.env.NODE_ENV}`);
 }
 
 bootstrap().catch(console.error);
